f3ae1fc3274f3c8ffc728c075939244eb0df7359
Mayck <mayck@MacBook-Pro-de-Mayck.local>
2020-05-25 16:08:07 -0300

    Rendering Contentful Posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,19 +9,16 @@ import blogStyle from './blog.module.scss'
 const BlogPage = () => {
         const data = useStaticQuery(graphql`
             query {
-                allMarkdownRemark {
-                    edges {
-                        node {
-                            frontmatter {
-                                title
-                                date
-                            }
-                            fields {
-                                slug 
-                            }
-                        }
-                    }
-                }
+               allContentfulBlogPost ( sort: {  fields: publishedDate, order: DESC }) {
+                   edges {
+                       node {
+                           title
+                           slug
+                           publishedDate(formatString: "MMMM Do, YYYY")
+                       }
+                   }
+
+               }
             }
         `)
 
@@ -29,12 +26,12 @@ const BlogPage = () => {
         <Layout>
         <h1>Blog</h1>
         <ol class={blogStyle.posts}>
-            {data.allMarkdownRemark.edges.map((edge) =>{
+            {data.allContentfulBlogPost.edges.map((edge) =>{
                 return (
                     <li class={blogStyle.post}>
-                        <Link to={`/blog/${edge.node.fields.slug}`}>
-                        <h2>{edge.node.frontmatter.title}</h2>
-                        <p>{edge.node.frontmatter.date}</p>
+                        <Link to={`/blog/${edge.node.slug}`}>
+                        <h2>{edge.node.title}</h2>
+                        <p>{edge.node.publishedDate}</p>
                         </Link>
                     </li>
                 )
